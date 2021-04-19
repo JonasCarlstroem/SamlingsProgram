df733916c89801401c5185d3072913fa903169db
JonasCarlstroem <82809067+JonasCarlstroem@users.noreply.github.com>
2021-04-19 12:57:39 +0200

    Add files via upload

---
+++ SamlingsProgram/SamlingsProgram/Calculations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamlingsProgram
+{
+    class Calculations
+    {
+
+        public int[] SolutionOne(int length)
+        {
+            int[] calc = new int[length];
+            int[] fCalc = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                calc[i] = i + 1;
+                if (calc[i] % 3 == 0 || calc[i] % 5 == 0)
+                {
+                    fCalc[i] = calc[i];
+                }
+            }
+            return fCalc;
+        }
+        public int[] SolutionTwo(int length)
+        {
+            int n1 = 0, n2 = 1, n3;
+            int[] fCalc = new int[length];
+            for (int i = 1; i < length; i++)
+            {
+                n3 = n1 + n2;
+                fCalc[i] = n3;
+                n1 = n2;
+                n2 = n3;
+            }
+                return fCalc;
+        }
+    }
+}
---
+++ SamlingsProgram/SamlingsProgram/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SamlingsProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void solveOne(object sender, RoutedEventArgs e)
+        {
+            Calculations calc = new Calculations();
+            if (solOneInput.Text.Length > 0)
+            {
+                OutputText.Text = string.Empty;
+                int[] newCalc = calc.SolutionOne(Convert.ToInt32(solOneInput.Text));
+                int totalSum = 0;
+                for (int i = 0; i < newCalc.Length; i++)
+                {
+                    totalSum += newCalc[i];
+                    if (newCalc[i] != 0)
+                        OutputText.Text += newCalc[i] + "\n";
+                }
+                
+                OutputText.Text += "Totalsumma: " + totalSum;
+            }
+        }
+
+        private void solveTwo_Click(object sender, RoutedEventArgs e)
+        {
+            Calculations calc = new Calculations();
+            if (solTwoInput.Text.Length > 0)
+            {
+                OutputText.Text = string.Empty;
+                int[] newCalc = calc.SolutionTwo(Convert.ToInt32(solTwoInput.Text));
+                int totalSum = 0;
+                for(int i = 0;i < newCalc.Length;i++)
+                {
+                    totalSum += newCalc[i];
+                    if (newCalc[i] != 0)
+                        OutputText.Text += newCalc[i] + "\r\n";
+                }
+                OutputText.Text += "Totalsumma: " + totalSum;
+            }
+        }
+    }
+}
